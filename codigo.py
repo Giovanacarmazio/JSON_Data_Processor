df169bb655580d374fccbc6db037968cc08a9fd3
Giovana Carmazio <83242981+Giovanacarmazio@users.noreply.github.com>
2021-10-31 19:59:36 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -27,3 +27,32 @@ for entry in entries:
         sexo[sexo_valor] += 1
 
 print(sexo)
+
+entries_normalize = pd.json_normalize(entries)
+"""
+{
+    "a": {
+        "b": 3,
+        "c": {...}
+    },
+    "h": 10
+}
+{
+    "a.b": 3,
+    "a.c": 5,
+    "h": 10
+}
+"""
+
+entries_df = pd.DataFrame.from_dict(entries_normalize)
+
+#4.1
+entries_df['_source.estabelecimento_uf'].unique()
+
+#4.2
+entries_df['_source.vacina_fabricante_nome'].unique()
+
+#4.3
+entries_df['_source.paciente_enumSexoBiologico'].value_counts()
+
+
